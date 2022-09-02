d79870da393692af5890409288f556026cd6f257
김주용 <22100181@handong.ac.kr>
2022-09-02 10:19:20 +0900

    create Hello Project

---
+++ src/main/java/org/example/Word.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Word {
+    private String name;
+    private String meaning;
+
+    public Word() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+}
